181c2e0985e3d21dfaea602e15d5a7f5a1c989b0
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-12-04 14:55:44 +0530

    Lifecycle of a Type-113 tx.

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -6,11 +6,28 @@ import {Transaction} from
     "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 
 /**
+ * @title ZkMinimalAccount
+ * @notice A minimal account contract for the zkSync system.
+ * @dev This contract is used to validate and execute transactions in the zkSync system.
+ * It is a part of the system contract architecture.
+ * The contract is used in two phases: validation and execution.
+ *
+ * Lifecycle of a type 113 (0x71) transaction
+ * msg.sender is the bootloader of the system contract
+ *
+ * The contract is used in two phases:
  * Phase 1: Validation
  * 1. The user sends the transaction to the zksync API client. (sort of a "light node")
  * 2. The zksync API client checks to see the nonce is unique by querring the nonceHolder system contract.
+ * 3. The zksync API client calls validateTransaction, which MUST update the nonce.
+ * 4. The zksync API client checks the nonce is updated.
+ * 5. The zksync API client calls payForTransaction, or prepareForTransaction & validatAndPayForPaymasterTransaction
+ * 6. The zksync APIL client varifies that the bootloader gets paid.
  *
  * Phase 2: Execution
+ * 1. The zksync API client passes the validated transaction to the main node / sequencer. (as of today, they are the same)
+ * 2. The main node calls excuteTransaction.
+ * 3. If a paymaster was used, the postTransaction is called
  */
 contract ZkMinimalAccount is IAccount {
     function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
